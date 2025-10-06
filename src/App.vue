d99b0bd02084f55537c52ac9b2ed3f49caec4407
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 19:03:56 +0100

    added placeholders for profile details

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
+<template>
+  <div>
+    <h2>
+      <a :href="repoStore.user?.html_url" target="_blank">{{ repoStore.username }}</a>
+      <img :src="repoStore.user?.avatar_url" alt="user_avatar" />
+    </h2>
+    <AppTable></AppTable>
+    <pre>{{ repoStore.repoJson }}</pre>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useRepoStore } from '@/stores/useRepoStore'
+import AppTable from './components/AppTable.vue'
+
+defineOptions({
+  name: 'App',
+})
 
 const repoStore = useRepoStore()
 
 onMounted(() => {
   repoStore.loadRepos('JacAndre')
+  repoStore.loadUserInfo('JacAndre')
 })
 </script>
-
-<template>
-  <div>
-    <h2>Username: {{ repoStore.currentUsername }}</h2>
-    <pre>{{ repoStore.repoJson }}</pre>
-    <div>{{ repoStore.readme }}</div>
-  </div>
-</template>
 
 <style scoped></style>
