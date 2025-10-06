1c968a670848b4cc80c7f015702ea387a9ba3b4b
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 18:00:33 +0100

    setting up API routes

---
+++ src/services/githubService.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    Accept: 'application/vnd.github.v3+json',
+  },
+})
+
+export const fetchReposByUser = async (username: string) => {
+  const response = await github.get(`/users/${username}/repos`)
+  return response.data
+}
+
+export const fetchReadme = async (owner: string, repo: string) => {
+  const response = await github.get(`/repos/${owner}/${repo}/readme`, {
+    headers: { Accept: 'application/vnd.github.v3.raw' },
+  })
+  return response.data
+}
