f27a54668ffec7a0f9b9f4a9bdb8183b9821b368
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 19:03:18 +0100

    adding user information to store, and creating new function to fetch user information

--- src/stores/useRepoStore.ts
+++ src/stores/useRepoStore.ts
@@ -1,38 +1,51 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
-import { fetchReadme, fetchReposByUser } from '@/services/githubService'
+import { fetchReadme, fetchReposByUser, fetchUserInfo } from '@/services/githubService'
 import type { Repo } from '@/types/repo'
+import type { User } from '@/types/user'
 
 export const useRepoStore = defineStore('repo', () => {
   const username = ref<string>('')
   const repos = ref<Repo[]>([])
-  const loading = ref<boolean>(false)
+  const selectedRepo = ref<Repo | null>(null)
+  const user = ref<User | null>(null)
+
   const readme = ref<string>('')
+  const loading = ref<boolean>(false)
 
   const repoJson = computed(() => repos.value)
   const currentUsername = computed(() => username.value)
+
+  async function loadUserInfo(presetUser: string): Promise<void> {
+    loading.value = true
+    user.value = await fetchUserInfo(presetUser)
+    loading.value = false
+  }
 
   async function loadRepos(presetUser: string): Promise<void> {
     username.value = presetUser
     loading.value = true
     repos.value = await fetchReposByUser(presetUser)
     loading.value = false
-    loadReadme(presetUser, repos.value[repos.value.length - 2]?.name || '')
   }
 
-  async function loadReadme(owner: string, repo: string): Promise<void> {
+  async function loadReadme(repo: Repo) {
     loading.value = true
-    readme.value = await fetchReadme(owner, repo)
+    selectedRepo.value = repo
+    readme.value = await fetchReadme(repo.owner.login, repo.name)
     loading.value = false
   }
 
   return {
     username,
     repos,
+    user,
     loading,
     readme,
     repoJson,
     currentUsername,
+    loadUserInfo,
     loadRepos,
+    loadReadme,
   }
 })
