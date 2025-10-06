006e98218958b5a85918c6fbd551fad7fd18d4ac
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 19:02:19 +0100

    creating interface for owner information

--- src/types/repo.ts
+++ src/types/repo.ts
@@ -1,9 +1,13 @@
+import type { Owner } from './owner'
+
 export interface Repo {
   id: number
   name: string
+  owner: Owner
   html_url: string
   description: string
   language: string
   stargazers_count: number
   forks_count: number
+  open_issues_count: number
 }
