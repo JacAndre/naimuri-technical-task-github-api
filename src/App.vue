d219486bfbd7faddbc3c29d446e662d1c5182218
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 21:14:26 +0100

    created profile card for user information

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
-  <div>
-    <h2>
-      <a :href="repoStore.user?.html_url" target="_blank">{{ repoStore.username }}</a>
-      <img :src="repoStore.user?.avatar_url" alt="user_avatar" />
-    </h2>
+  <v-toolbar title="Naimuri Technical Challenge: GitHub UI" color="pink-darken-1"></v-toolbar>
+
+  <v-container>
+    <div class="position-relative d-inline-block">
+      <div
+        class="d-flex align-center mb-0"
+        @mouseenter="showOverlay = true"
+        @mouseleave="showOverlay = false"
+      >
+        <a :href="repoStore.user?.html_url" target="_blank" class="text-decoration-none">
+          <v-avatar class="me-3">
+            <v-img alt="Avatar" :src="repoStore.user?.avatar_url" />
+          </v-avatar>
+          <span class="text-h5">{{ repoStore.username }}</span>
+        </a>
+
+        <UserProfileOverlay v-if="showOverlay" :user="repoStore.user" />
+      </div>
+    </div>
+
     <AppTable></AppTable>
-    <pre>{{ repoStore.repoJson }}</pre>
-  </div>
+    <pre>{{ repoStore.repos }}</pre>
+  </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRepoStore } from '@/stores/useRepoStore'
 import AppTable from './components/AppTable.vue'
+import UserProfileOverlay from './components/UserProfileOverlay.vue'
 
 defineOptions({
   name: 'App',
 })
 
 const repoStore = useRepoStore()
+const showOverlay = ref(false)
 
 onMounted(() => {
   repoStore.loadRepos('JacAndre')
