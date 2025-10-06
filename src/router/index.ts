12fda27501a2ffc9e17bd72a636fabae58565044
Jacques Andre <jacques_andre@ymail.com>
2025-10-06 18:59:28 +0100

    handling path warning

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from '@/App.vue'
+
+const routes = [{ path: '/', name: 'Home', component: App }]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
