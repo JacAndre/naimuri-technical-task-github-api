33aac6f1050fe4852d6eaf9dbca5d3b83c9ba1c0
Jacques Andre <jacques_andre@ymail.com>
2025-10-07 10:07:27 +0100

    added error message snackbar to application

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
+  <v-snackbar v-model="repoStore.snackbar" color="error" timeout="4000">
+    {{ repoStore.snackbarMessage }}
+  </v-snackbar>
+
   <v-toolbar title="Naimuri Technical Challenge: GitHub UI" color="pink-darken-1"></v-toolbar>
 
   <v-container>
     <div class="position-relative d-inline-block">
-      <div
-        class="d-flex align-center mb-0"
-        @mouseenter="showOverlay = true"
-        @mouseleave="showOverlay = false"
-      >
+      <div class="mb-0" @mouseenter="showOverlay = true" @mouseleave="showOverlay = false">
         <a :href="repoStore.user?.html_url" target="_blank" class="text-decoration-none">
-          <v-avatar class="me-3">
-            <v-img alt="Avatar" :src="repoStore.user?.avatar_url" />
-          </v-avatar>
-          <span class="text-h5">{{ repoStore.username }}</span>
+          <div class="d-flex align-center">
+            <v-avatar class="me-3">
+              <v-img alt="Avatar" :src="repoStore.user?.avatar_url" />
+            </v-avatar>
+            <span class="text-h5">{{ repoStore.username }}</span>
+          </div>
         </a>
 
         <UserProfileOverlay v-if="showOverlay" :user="repoStore.user" />
