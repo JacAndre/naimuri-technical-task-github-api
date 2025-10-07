98aa60489273af9e554fbca8a4d52c10db222684
Jacques Andre <jacques_andre@ymail.com>
2025-10-07 12:08:09 +0100

    added github markdown for nicer readme viewing

--- src/components/ReadmeViewer.vue
+++ src/components/ReadmeViewer.vue
@@ -1,15 +1,18 @@
 <template>
   <v-sheet elevation="1" class="pa-4" style="max-height: 60vh; overflow-y: auto; overflow-x: auto">
     <v-progress-linear v-if="loading" indeterminate color="primary" height="4" />
-    <pre v-else>{{ content }}</pre>
+    <div v-else class="markdown-body" v-html="renderedMarkdown" />
   </v-sheet>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue'
+import { marked } from 'marked'
+
+const props = defineProps<{
   content: string
   loading?: boolean
 }>()
-</script>
 
-<style scoped></style>
+const renderedMarkdown = computed(() => marked.parse(props.content || ''))
+</script>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
+import 'github-markdown-css/github-markdown.css'
 
 const app = createApp(App)
 
