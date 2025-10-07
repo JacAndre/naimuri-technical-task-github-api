b291d44f63391eeb9e7bf16a01f85064d27d15a9
Jacques Andre <jacques_andre@ymail.com>
2025-10-07 12:03:10 +0100

    viewer styling

--- src/components/ReadmeViewer.vue
+++ src/components/ReadmeViewer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="readme-container">
+  <v-sheet elevation="1" class="pa-4" style="max-height: 60vh; overflow-y: auto; overflow-x: auto">
     <v-progress-linear v-if="loading" indeterminate color="primary" height="4" />
     <pre v-else>{{ content }}</pre>
-  </div>
+  </v-sheet>
 </template>
 
 <script setup lang="ts">
@@ -12,13 +12,4 @@ defineProps<{
 }>()
 </script>
 
-<style scoped>
-.readme-container {
-  max-width: 100%;
-  overflow-x: auto;
-  white-space: pre;
-  font-family: monospace;
-  font-size: 0.875rem;
-  line-height: 1.4;
-}
-</style>
+<style scoped></style>
