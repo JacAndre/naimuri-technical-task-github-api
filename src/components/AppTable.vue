0a72dde42cdc5b2dcb990c430e3f2877ea8e54cd
Jacques Andre <jacques_andre@ymail.com>
2025-10-07 12:02:55 +0100

    converted dropdowns to dialogs

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,35 +1,41 @@
 <template>
-  <v-data-table
-    :items="repoStore.repos"
-    :headers="headers"
-    :loading="repoStore.loadingRepos"
-    item-value="id"
-    class="elevation-1 mt-4"
-  >
-    <template #item="{ item }">
-      <tr>
-        <td>
-          <a :href="item.html_url" target="_blank">{{ item.name }}</a>
-        </td>
-        <td>{{ item.language }}</td>
-        <td>{{ item.stargazers_count }}</td>
-        <td>{{ item.forks_count }}</td>
-        <td>{{ item.open_issues_count }}</td>
-        <td>
-          <v-btn size="small" @click="toggleReadme(item.id)">
-            {{ selectedRepoId === item.id ? 'Hide' : 'View' }} README
-          </v-btn>
-        </td>
-      </tr>
-      <tr v-if="selectedRepoId === item.id">
-        <td colspan="6">
-          <v-card class="ma-4 pa-4" elevation="0" variant="outlined">
-            <ReadmeViewer :content="repoStore.readme" />
-          </v-card>
-        </td>
-      </tr>
-    </template>
-  </v-data-table>
+  <div ref="tableContainer">
+    <v-data-table
+      :items="repoStore.repos"
+      :headers="headers"
+      :loading="repoStore.loadingRepos"
+      item-value="id"
+      class="elevation-1 mt-4"
+    >
+      <template #item="{ item }">
+        <tr>
+          <td>
+            <a :href="item.html_url" target="_blank">{{ item.name }}</a>
+          </td>
+          <td>{{ item.language }}</td>
+          <td>{{ item.stargazers_count }}</td>
+          <td>{{ item.forks_count }}</td>
+          <td>{{ item.open_issues_count }}</td>
+          <td>
+            <v-btn size="small" @click="openReadme(item)"> View README </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="showDialog" max-width="900">
+      <v-card>
+        <v-card-title class="text-h6"> {{ selectedRepo?.name }} README </v-card-title>
+        <v-card-text>
+          <ReadmeViewer :content="repoStore.readme" :loading="repoStore.loadingReadme" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text @click="showDialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -37,12 +43,14 @@ import { useRepoStore } from '@/stores/useRepoStore'
 import { ref } from 'vue'
 import ReadmeViewer from './ReadmeViewer.vue'
 import type { DataTableHeader } from 'vuetify'
+import type { Repo } from '@/types/repo'
 
 const repoStore = useRepoStore()
-const selectedRepoId = ref<number | null>(null)
+const selectedRepo = ref<Repo | null>(null)
+const showDialog = ref(false)
 
 const headers: DataTableHeader[] = [
-  { title: 'Name', key: 'name', sortable: true },
+  { title: 'Name', key: 'name', sortable: true, width: '30%' },
   { title: 'Language', key: 'language', sortable: true },
   { title: 'Stars', key: 'stargazers_count', sortable: true },
   { title: 'Forks', key: 'forks_count', sortable: true },
@@ -50,12 +58,10 @@ const headers: DataTableHeader[] = [
   { title: 'Actions', key: 'actions', sortable: false },
 ]
 
-function toggleReadme(repoId: number) {
-  selectedRepoId.value = selectedRepoId.value === repoId ? null : repoId
-  const repo = repoStore.repos.find((r) => r.id === repoId)
-  if (repo) {
-    repoStore.loadReadme(repo)
-  }
+function openReadme(repo: Repo) {
+  selectedRepo.value = repo
+  repoStore.loadReadme(repo)
+  showDialog.value = true
 }
 </script>
 
