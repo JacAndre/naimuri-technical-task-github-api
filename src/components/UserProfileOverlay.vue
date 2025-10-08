56276baaad0f8148494db12ba868b0ea4104191d
Jacques Andre <jacques_andre@ymail.com>
2025-10-08 11:37:59 +0100

    adding aria labels to profile overlay

--- src/components/UserProfileOverlay.vue
+++ src/components/UserProfileOverlay.vue
@@ -1,21 +1,49 @@
 <template>
-  <v-card v-if="user" elevation="10" class="pa-4 profile-overlay" variant="flat">
-    <div class="text-h6 mb-2">
+  <v-card
+    v-if="user"
+    elevation="10"
+    class="pa-4 profile-overlay"
+    variant="flat"
+    role="region"
+    :aria-label="`GitHub profile card for ${user.name}`"
+  >
+    <div class="text-h6 mb-2" role="heading" aria-level="2">
       {{ user.name }}
     </div>
-    <div v-if="user.bio" class="mb-2">
+
+    <div v-if="user.bio" class="mb-2" :aria-label="`Bio: ${user.bio}`">
       {{ user.bio }}
     </div>
-    <v-divider class="mb-2"></v-divider>
-    <div v-if="user.location" class="mb-2">
-      <v-icon class="mr-2">mdi-map-marker</v-icon>
+
+    <v-divider class="mb-2" aria-hidden="true"></v-divider>
+
+    <div
+      v-if="user.location"
+      class="mb-2"
+      role="contentinfo"
+      :aria-label="`Location: ${user.location}`"
+    >
+      <v-icon class="mr-2" aria-hidden="true">mdi-map-marker</v-icon>
       {{ user.location }}
     </div>
-    <div v-if="user.followers !== undefined">
-      <v-icon class="mr-2">mdi-account-group</v-icon>
+
+    <div
+      v-if="user.followers !== undefined"
+      role="contentinfo"
+      :aria-label="`${user.followers} followers and ${user.following} following`"
+    >
+      <v-icon class="mr-2" aria-hidden="true">mdi-account-group</v-icon>
       {{ user.followers }} followers · {{ user.following }} following
     </div>
-    <v-btn variant="text" :href="user.html_url" target="_blank" size="small" class="mt-2">
+
+    <v-btn
+      variant="text"
+      :href="user.html_url"
+      target="_blank"
+      size="small"
+      class="mt-2"
+      aria-label="View GitHub profile in a new tab"
+    >
       View GitHub
     </v-btn>
   </v-card>
