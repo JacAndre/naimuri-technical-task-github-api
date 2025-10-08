54f028d75beaa5c875f632add772ebb72db30f54
Jacques Andre <jacques_andre@ymail.com>
2025-10-08 11:37:20 +0100

    adding copy content to viewer

--- src/components/ReadmeViewer.vue
+++ src/components/ReadmeViewer.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-sheet elevation="1" class="pa-4" style="max-height: 60vh; overflow-y: auto; overflow-x: auto">
+  <v-sheet elevation="1" class="pa-4" style="max-height: 600px; overflow-y: auto; overflow-x: auto">
+    <v-btn
+      icon
+      size="small"
+      class="position-absolute"
+      style="top: 16px; right: 16px; z-index: 1"
+      @click="copyContent"
+    >
+      <v-icon>mdi-content-copy</v-icon>
+    </v-btn>
     <v-progress-linear v-if="loading" indeterminate color="primary" height="4" />
     <div v-else class="markdown-body" v-html="renderedMarkdown" />
   </v-sheet>
@@ -15,4 +24,15 @@ const props = defineProps<{
 }>()
 
 const renderedMarkdown = computed(() => marked.parse(props.content || ''))
+
+function copyContent() {
+  navigator.clipboard
+    .writeText(props.content)
+    .then(() => {
+      console.log('README copied to clipboard')
+    })
+    .catch((err) => {
+      console.error('Copy failed:', err)
+    })
+}
 </script>
