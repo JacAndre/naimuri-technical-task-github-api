e2e08716968be1e221a035ca8ef5a76ec7d28aac
Jacques Andre <jacques_andre@ymail.com>
2025-10-08 11:36:32 +0100

    Better type handling in store, added suggestions consts for searching users

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,3 +1,7 @@
+import type { Rate, RateLimit } from '@/types/rateLimit'
+import type { Repo } from '@/types/repo'
+import type { SuggestedUser } from '@/types/suggestedUser'
+import type { User } from '@/types/user'
 import axios from 'axios'
 
 const github = axios.create({
@@ -8,9 +12,23 @@ const github = axios.create({
   },
 })
 
-export const fetchUserInfo = async (username: string) => {
+export const fetchUserSuggestions = async (query: string): Promise<SuggestedUser[]> => {
   try {
-    const response = await github.get(`/users/${username}`)
+    const response = await github.get<{ items: SuggestedUser[] }>('/search/users', {
+      params: { q: query },
+    })
+    return response.data.items.map((user) => ({
+      login: user.login,
+      avatar_url: user.avatar_url,
+    }))
+  } catch {
+    return []
+  }
+}
+
+export const fetchUserInfo = async (username: string): Promise<User> => {
+  try {
+    const response = await github.get<User>(`/users/${username}`)
     return response.data
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
@@ -20,31 +38,42 @@ export const fetchUserInfo = async (username: string) => {
   }
 }
 
-export const fetchReposByUser = async (username: string) => {
+export const fetchReposByUser = async (username: string): Promise<Repo[]> => {
   try {
-    const response = await github.get(`/users/${username}/repos`)
+    const response = await github.get<Repo[]>(`/users/${username}/repos`)
     return response.data
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
+      if (error.response?.status === 404) {
+        throw new Error('No User Information found for: ' + username)
+      }
       throw new Error(error.response?.data?.message || 'Failed to fetch repositories')
     }
     throw new Error('Unexpected error occurred')
   }
 }
 
-export const fetchReadme = async (owner: string, repo: string) => {
+export const fetchReadme = async (owner: string, repo: string): Promise<string> => {
   try {
-    const response = await github.get(`/repos/${owner}/${repo}/readme`, {
+    const response = await github.get<string>(`/repos/${owner}/${repo}/readme`, {
       headers: { Accept: 'application/vnd.github.v3.raw' },
     })
     return response.data
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
-      if (error?.response?.status === 404) {
+      if (error.response?.status === 404) {
         return 'README not found for this repository.'
       }
-      throw new Error(error?.response?.data?.message || 'Failed to fetch README')
+      throw new Error(error.response?.data?.message || 'Failed to fetch README')
     }
     throw new Error('Unexpected error occurred')
   }
+}
+
+export const fetchRateLimit = async (): Promise<Rate> => {
+  const response = await github.get<RateLimit>(`/rate_limit`)
+  if (response.data?.rate.remaining === 0) {
+    throw new Error('GitHub API rate limit exceeded. Please wait until reset.')
+  }
+  return response.data?.rate
 }
